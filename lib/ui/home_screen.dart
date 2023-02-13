e15a8de0d37de2ee96004a3ff250360673458342
saengle <zohv817@gmail.com>
2023-02-13 13:45:05 +0900

    Add: model class, UI

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -10,6 +10,49 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          '이미지 검색',
+          style: TextStyle(color: Colors.black),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                suffixIcon: IconButton(
+                    onPressed: () {}, icon: const Icon(Icons.search)),
+                border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10))),
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+                padding: const EdgeInsets.all(16),
+                itemCount: 10,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16),
+                itemBuilder: (context, index) {
+                  return Container(
+                    decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(16)),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                'https://img.vogue.co.kr/vogue/2021/02/style_6020aef125358.jpg'))),
+                  );
+                }),
+          )
+        ],
+      ),
+    );
   }
 }
