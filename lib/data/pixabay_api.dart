a97a5d7ec0e02ba291d623544b64e1d9d57083fa
saengle <zohv817@gmail.com>
2023-02-14 17:42:11 +0900

    Add: pixabay_api_test.dart
    http를 이용한 pixabay_api 테스트 추가

--- lib/data/pixabay_api.dart
+++ lib/data/pixabay_api.dart
@@ -11,8 +11,12 @@ class PixabayApi implements PhotoApiRepository {
   final baseUrl = 'https://pixabay.com/api/';
 
   @override
-  Future<List<Photo>> fetch(String query) async {
-    final response = await http.get(Uri.parse(
+  Future<List<Photo>> fetch(String query, {http.Client? client}) async {
+    // if(client == null) {client = http.Client()}
+    // 아래 코드 같은 내용 (줄여쓰기)
+    client ??= http.Client();
+    final response = await client.get(Uri.parse(
+      // http.get -> client.get 변경  : client 가 있으면 client를 사용, 없을시 기본 http.Client(가 원래 기본값)를 사용
         '$baseUrl?key=${Env.Image_Key}&q=$query&image_type=photo&pretty=true'));
 
     Map<String, dynamic> jsonResponse = jsonDecode(response.body);
