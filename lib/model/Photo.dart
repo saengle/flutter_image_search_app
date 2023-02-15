49cd5357681643bc006d4dcf51d6adb8ab9513c8
saengle <zohv817@gmail.com>
2023-02-15 14:17:08 +0900

    Fix: Photo model
    JsonSerializable이용하여 모델 클래스 만들기.
    Equatable 이용하여 아이디 기준으로 같으면 같다고 알려주게 (테스트 용이)

--- lib/model/Photo.dart
+++ lib/model/Photo.dart
@@ -1,113 +1,21 @@
-class Photo {
-  int? id;
-  String? pageURL;
-  String? type;
-  String? tags;
-  String? previewURL;
-  int? previewWidth;
-  int? previewHeight;
-  String? webformatURL;
-  int? webformatWidth;
-  int? webformatHeight;
-  String? largeImageURL;
-  int? imageWidth;
-  int? imageHeight;
-  int? imageSize;
-  int? views;
-  int? downloads;
-  int? collections;
-  int? likes;
-  int? comments;
-  int? userId;
-  String? user;
-  String? userImageURL;
+import 'package:json_annotation/json_annotation.dart';
+import 'package:equatable/equatable.dart';
 
-  Photo(
-      {this.id,
-      this.pageURL,
-      this.type,
-      this.tags,
-      this.previewURL,
-      this.previewWidth,
-      this.previewHeight,
-      this.webformatURL,
-      this.webformatWidth,
-      this.webformatHeight,
-      this.largeImageURL,
-      this.imageWidth,
-      this.imageHeight,
-      this.imageSize,
-      this.views,
-      this.downloads,
-      this.collections,
-      this.likes,
-      this.comments,
-      this.userId,
-      this.user,
-      this.userImageURL});
+part 'Photo.g.dart';
 
-  Photo.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    pageURL = json['pageURL'];
-    type = json['type'];
-    tags = json['tags'];
-    previewURL = json['previewURL'];
-    previewWidth = json['previewWidth'];
-    previewHeight = json['previewHeight'];
-    webformatURL = json['webformatURL'];
-    webformatWidth = json['webformatWidth'];
-    webformatHeight = json['webformatHeight'];
-    largeImageURL = json['largeImageURL'];
-    imageWidth = json['imageWidth'];
-    imageHeight = json['imageHeight'];
-    imageSize = json['imageSize'];
-    views = json['views'];
-    downloads = json['downloads'];
-    collections = json['collections'];
-    likes = json['likes'];
-    comments = json['comments'];
-    userId = json['user_id'];
-    user = json['user'];
-    userImageURL = json['userImageURL'];
-  }
+@JsonSerializable()
+class Photo extends Equatable {
+  final int id;
+  final String tags;
+  final String previewURL;
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['pageURL'] = this.pageURL;
-    data['type'] = this.type;
-    data['tags'] = this.tags;
-    data['previewURL'] = this.previewURL;
-    data['previewWidth'] = this.previewWidth;
-    data['previewHeight'] = this.previewHeight;
-    data['webformatURL'] = this.webformatURL;
-    data['webformatWidth'] = this.webformatWidth;
-    data['webformatHeight'] = this.webformatHeight;
-    data['largeImageURL'] = this.largeImageURL;
-    data['imageWidth'] = this.imageWidth;
-    data['imageHeight'] = this.imageHeight;
-    data['imageSize'] = this.imageSize;
-    data['views'] = this.views;
-    data['downloads'] = this.downloads;
-    data['collections'] = this.collections;
-    data['likes'] = this.likes;
-    data['comments'] = this.comments;
-    data['user_id'] = this.userId;
-    data['user'] = this.user;
-    data['userImageURL'] = this.userImageURL;
-    return data;
-  }
+  Photo({required this.id, required this.tags, required this.previewURL});
 
+  factory Photo.fromJson(Map<String, dynamic> json) => _$PhotoFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PhotoToJson(this);
+
+  //id 기준으로 같으면 같다고 알려주기 (이쿼터블 이용)
   @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is Photo && runtimeType == other.runtimeType && id == other.id;
-
-  @override
-  int get hashCode => id.hashCode;
-
-  @override
-  String toString() {
-    return 'Photo{id: $id}';
-  }
+  List<Object?> get props => [id];
 }
